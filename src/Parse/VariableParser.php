5fd3856159c89b985716f7ca53a2e8253c87da23
hirokinoue <70567194+hirokinoue@users.noreply.github.com>
2025-03-16 20:35:34 +0900

    FunctionLikeを分析対象にする

--- src/Parse/VariableParser.php
+++ src/Parse/VariableParser.php
@@ -5,15 +5,10 @@ declare(strict_types=1);
 namespace Smeghead\PhpVariableHardUsage\Parse;
 
 use PhpParser\Node\Expr\Variable;
-use PhpParser\Node\Stmt;
-use PhpParser\Node\Stmt\ClassMethod;
-use PhpParser\Node\Stmt\Class_;
-use PhpParser\Node\Stmt\Function_;
-use PhpParser\NodeDumper;
-use PhpParser\NodeVisitor\FindingVisitor;
+use PhpParser\Node\FunctionLike;
+use PhpParser\NodeFinder;
 use PhpParser\Parser;
 use Smeghead\PhpVariableHardUsage\Parse\Exception\ParseFailedException;
-
 final class VariableParser
 {
     private Parser $parser;
@@ -23,96 +18,6 @@ final class VariableParser
         $this->parser = (new \PhpParser\ParserFactory())->createForNewestSupportedVersion();
     }
 
-    /**
-     * @param list<Stmt> $stmt
-     * @return list<Function_>
-     */
-    private function getFunctions(array $stmt): array
-    {
-        $functionVisitor = new FindingVisitor(function ($node) {
-            return $node instanceof Function_;
-        });
-        $traverser = new \PhpParser\NodeTraverser();
-        $traverser->addVisitor($functionVisitor);
-        $traverser->traverse($stmt);
-
-        return $functionVisitor->getFoundNodes(); // @phpstan-ignore-line
-    }
-
-    /**
-     * @param Function_|ClassMethod $function
-     * @return list<Variable>
-     */
-    private function getVariables(Function_|ClassMethod $function): array
-    {
-        $variableVisitor = new FindingVisitor(function ($node) {
-            return $node instanceof Variable;
-        });
-        $traverser = new \PhpParser\NodeTraverser();
-        $traverser->addVisitor($variableVisitor);
-        $traverser->traverse([$function]);
-
-        return $variableVisitor->getFoundNodes(); // @phpstan-ignore-line
-    }
-
-    /**
-     * @param list<Stmt> $stmts
-     * @return list<Func>
-     */
-    private function parseFunctions(array $stmts): array
-    {
-        $foundFunctions = $this->getFunctions($stmts);
-
-        $functions = [];
-        foreach ($foundFunctions as $foundFunction) {
-            $variables = $this->getVariables($foundFunction);
-            $func = new Func($foundFunction->name->name);
-            foreach ($variables as $variable) {
-                $func->addVariable(new VarReference($variable->name, $variable->getLine())); // @phpstan-ignore-line
-            }
-            $functions[] = $func;
-        }
-        return $functions;
-    }
-
-    /**
-     * @param list<Stmt> $stmt
-     * @return list<Class_>
-     */
-    private function getClasses(array $stmt): array
-    {
-        $classVisitor = new FindingVisitor(function ($node) {
-            return $node instanceof \PhpParser\Node\Stmt\Class_;
-        });
-        $traverser = new \PhpParser\NodeTraverser();
-        $traverser->addVisitor($classVisitor);
-        $traverser->traverse($stmt);
-
-        return $classVisitor->getFoundNodes(); // @phpstan-ignore-line
-    }
-
-    /**
-     * @param list<Stmt> $stmts
-     * @return list<Func>
-     */
-    private function parseClasses(array $stmts): array
-    {
-        $foundClasses = $this->getClasses($stmts);
-
-        $methods = [];
-        foreach ($foundClasses as $foundClass) {
-            foreach ($foundClass->getMethods() as $method) {
-                $variables = $this->getVariables($method);
-                $func = new Func(sprintf('%s::%s', $foundClass->name, $method->name->name));
-                foreach ($variables as $variable) {
-                    $func->addVariable(new VarReference($variable->name, $variable->getLine())); // @phpstan-ignore-line
-                }
-                $methods[] = $func;
-            }
-        }
-        return $methods;
-    }
-
     public function parse(string $content): ParseResult
     {
         $stmts = $this->parser->parse($content);
@@ -120,7 +25,18 @@ final class VariableParser
             throw new ParseFailedException();
         }
 
-        $functions = $this->parseFunctions($stmts) + $this->parseClasses($stmts);
+        $nodeFinder = new NodeFinder();
+
+        $functionLikes = $nodeFinder->findInstanceOf($stmts, FunctionLike::class);
+
+        $functions = array_map(function (FunctionLike $function) use ($nodeFinder) {
+            $func = new Func($function->name->name);
+            $variables = $nodeFinder->findInstanceOf($function, Variable::class);
+            foreach ($variables as $variable) {
+                $func->addVariable(new VarReference($variable->name, $variable->getLine()));
+            }
+            return $func;
+        }, $functionLikes);
 
         return new ParseResult($functions);
     }
